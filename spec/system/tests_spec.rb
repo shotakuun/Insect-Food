58e443943f40c34267acf5d240c41be4829a69ba
Masaaki618 <masaaki_618@icloud.com>
2022-07-11 23:42:17 +0900

    rubocopの指摘事項の修正 #3

--- spec/system/tests_spec.rb
+++ spec/system/tests_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe "Tests", type: :system do
-  describe 'テストコントローラー'do
-    it "indexページへの遷移とページ内の文字が確認できること" do
+require 'rails_helper'
+
+RSpec.describe 'Tests', type: :system do
+  describe 'テストコントローラー' do
+    it 'indexページへの遷移とページ内の文字が確認できること' do
       visit test_path
-      expect(page).to have_content "テスト"
+      expect(page).to have_content 'テスト'
     end
   end
 end
