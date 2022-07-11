ee0c818e99e96f2c720d4d16c5ba63067f53e13b
Masaaki618 <masaaki_618@icloud.com>
2022-07-11 23:19:47 +0900

    記載ミスの修正 #3

--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,15 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.generators.template_engine = :slim
-    g.skip_routes true #routesの自動記述をoff
-    g.helper false #app/helperの自動生成をoff
-    g.assets false #同名のcssやjsの生成をoff
-    g.test_framework :rspec, #標準のテストをRSpecに変更
-                     controller_specs: false, #公式で非推奨のためoff
-                     view_specs: false, #Request/Systemで代替するのでoff
-                     routing_specs: false #使用頻度低いのでoff
-
+    config.generators do |g|
+      g.skip_routes true #routesの自動記述をoff
+      g.helper false #app/helperの自動生成をoff
+      g.assets false #同名のcssやjsの生成をoff
+      g.test_framework :rspec, #標準のテストをRSpecに変更
+                       controller_specs: false, #公式で非推奨のためoff
+                       view_specs: false, #Request/Systemで代替するのでoff
+                       routing_specs: false #使用頻度低いのでoff
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
