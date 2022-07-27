4322d2b15bdae4281c1c4cdf97d2a126476ffb1f
Masaaki618 <masaaki_618@icloud.com>
2022-07-27 20:34:36 +0900

    Add: バリデーションの追加#18

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@
 #  id               :bigint           not null, primary key
 #  crypted_password :string(255)
 #  email            :string(255)      not null
+#  name             :string(255)      not null
 #  salt             :string(255)
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
@@ -17,4 +18,7 @@
 #
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :email, uniqueness: true, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :name, uniqueness: true, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :password, presence: true
 end
