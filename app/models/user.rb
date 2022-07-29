ed664b88ff51240b8ff714e79221b321791f0a6f
Masaaki618 <masaaki_618@icloud.com>
2022-07-29 19:27:46 +0900

    Fix: バリデーションの修正#18

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,10 @@
 #
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 5 }, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true, presence: true, length: { minimum: 5, maximum: 255 }
-  validates :name, uniqueness: true, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :email, uniqueness: true, presence: true, length: { maximum: 255 }
+  validates :name, uniqueness: true, presence: true, length: { minimum: 3, maximum: 255 }
 end
