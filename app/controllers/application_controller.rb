b3eeda5d767763d1a2b9e18d83f69d88a0fbfa03
Masaaki618 <masaaki_618@icloud.com>
2022-07-30 08:58:14 +0900

    Add: 仮)ユーザー登録の実装#18

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  protected
+
+  def not_authenticated; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'top#index'
+  root to: "top#index"
+  get "/sign_up", to: "users#new"
+  post "/sign_up", to: "users#create"
 end
