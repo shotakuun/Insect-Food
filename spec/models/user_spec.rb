df3e78c218c9290be4d226f95f9baf5b5c37d3af
Masaaki618 <masaaki_618@icloud.com>
2022-07-29 19:33:39 +0900

    Fix: rubocopからの指摘事項を修正#18

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,86 +16,86 @@
 #
 #  index_users_on_email  (email) UNIQUE
 #
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:create_user) { create(:user) }
   let(:user) { build(:user) }
 
-  describe "Userモデルのバリデーションテスト" do
-    it "userが作成出来ること" do
+  describe 'Userモデルのバリデーションテスト' do
+    it 'userが作成出来ること' do
       expect(user).to be_valid
     end
 
-    describe "emailカラム" do
-      it "メールが空の場合無効であること" do
+    describe 'emailカラム' do
+      it 'メールが空の場合無効であること' do
         user = build(:user, email: nil)
         expect(user.valid?).to be false
-        expect(user.errors[:email]).to include("を入力してください")
+        expect(user.errors[:email]).to include('を入力してください')
       end
 
-      it "メールが既に登録されていたら無効であること" do
+      it 'メールが既に登録されていたら無効であること' do
         user = build(:user, email: create_user.email)
         expect(user.valid?).to be false
-        expect(user.errors[:email]).to include("はすでに存在します")
+        expect(user.errors[:email]).to include('はすでに存在します')
       end
 
-      it "メールが255文字以上の場合無効であること" do
-        user = build(:user, email: "a" * 256)
+      it 'メールが255文字以上の場合無効であること' do
+        user = build(:user, email: 'a' * 256)
         expect(user.valid?).to be false
-        expect(user.errors[:email]).to include("は255文字以内で入力してください")
+        expect(user.errors[:email]).to include('は255文字以内で入力してください')
       end
     end
 
-    describe "nameカラム" do
-      it "ニックネームが空の場合無効であること" do
+    describe 'nameカラム' do
+      it 'ニックネームが空の場合無効であること' do
         user = build(:user, name: nil)
         expect(user.valid?).to be false
-        expect(user.errors[:name]).to include("を入力してください")
+        expect(user.errors[:name]).to include('を入力してください')
       end
 
-      it "ニックネームが既に登録されていたら無効であること" do
+      it 'ニックネームが既に登録されていたら無効であること' do
         user = build(:user, name: create_user.name)
         expect(user.valid?).to be false
-        expect(user.errors[:name]).to include("はすでに存在します")
+        expect(user.errors[:name]).to include('はすでに存在します')
       end
 
-      it "ニックネームが3文字以内の場合無効であること" do
-        user = build(:user, name: "a" * 2)
+      it 'ニックネームが3文字以内の場合無効であること' do
+        user = build(:user, name: 'a' * 2)
         expect(user.valid?).to be false
-        expect(user.errors[:name]).to include("は3文字以上で入力してください")
+        expect(user.errors[:name]).to include('は3文字以上で入力してください')
       end
 
-      it "ニックネームが255文字以上の場合無効であること" do
-        user = build(:user, name: "a" * 256)
+      it 'ニックネームが255文字以上の場合無効であること' do
+        user = build(:user, name: 'a' * 256)
         expect(user.valid?).to be false
-        expect(user.errors[:name]).to include("は255文字以内で入力してください")
+        expect(user.errors[:name]).to include('は255文字以内で入力してください')
       end
     end
 
-    describe "パスワードカラム" do
-      it "パスワードが空の場合無効であること" do
+    describe 'パスワードカラム' do
+      it 'パスワードが空の場合無効であること' do
         user = build(:user, password: nil)
         expect(user.valid?).to be false
-        expect(user.errors[:password]).to include("を入力してください")
+        expect(user.errors[:password]).to include('を入力してください')
       end
 
-      it "パスワード確認が空の場合無効であること" do
+      it 'パスワード確認が空の場合無効であること' do
         user = build(:user, password_confirmation: nil)
         expect(user.valid?).to be false
-        expect(user.errors[:password_confirmation]).to include("を入力してください")
+        expect(user.errors[:password_confirmation]).to include('を入力してください')
       end
 
-      it "パスワードが3文字以内の場合無効であること" do
-        user = build(:user, password: "a" * 4)
+      it 'パスワードが3文字以内の場合無効であること' do
+        user = build(:user, password: 'a' * 4)
         expect(user.valid?).to be false
-        expect(user.errors[:password]).to include("は5文字以上で入力してください")
+        expect(user.errors[:password]).to include('は5文字以上で入力してください')
       end
 
-      it "パスワードとパスワード確認が一致しない場合無効であること" do
-        user = build(:user, password: "a" * 6, password_confirmation: "a" * 7)
+      it 'パスワードとパスワード確認が一致しない場合無効であること' do
+        user = build(:user, password: 'a' * 6, password_confirmation: 'a' * 7)
         expect(user.valid?).to be false
-        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+        expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
       end
     end
   end
