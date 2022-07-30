b065bb3bf7b9858cfba43f2c520cd9addb1958d9
Masaaki618 <masaaki_618@icloud.com>
2022-07-30 11:56:44 +0900

    Add: ログイン、ログアウト機能の実装#18

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "top#index"
-  get "/sign_up", to: "users#new"
-  post "/sign_up", to: "users#create"
+  root to: 'top#index'
+
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
