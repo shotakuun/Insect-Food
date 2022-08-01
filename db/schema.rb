5871f85f968e870e4c858ddbed42558cfbc453e6
Masaaki618 <masaaki_618@icloud.com>
2022-08-01 14:25:14 +0900

    Add: パスワードリセットのモジュールを追加#21

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_110732) do
+ActiveRecord::Schema.define(version: 2022_07_31_111755) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
@@ -19,7 +19,12 @@ ActiveRecord::Schema.define(version: 2022_07_27_110732) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
