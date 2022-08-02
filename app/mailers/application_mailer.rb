5409ad03e559810d81045594d8e27a1eee103ee6
Masaaki618 <masaaki_618@icloud.com>
2022-08-02 19:42:17 +0900

    Rem: rubocopの指摘事項の修正#25

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "insectfood.info@gmail.com"
-  layout "mailer"
+  default from: 'insectfood.info@gmail.com'
+  layout 'mailer'
 end
--- spec/mailers/thanx_mailer_spec.rb
+++ spec/mailers/thanx_mailer_spec.rb
@@ -1,26 +1,28 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe ThanxMailer, type: :mailer do
   let(:user) { build(:user) }
 
   before do
-    ActionMailer::Base.deliveries.clear #送られたテストが配列になって保存されているのでそれらをクリアする
-    ThanxMailer.with(name: user.name, to: user.email).welcome(user).deliver_now #メールを送る処理
+    ActionMailer::Base.deliveries.clear # 送られたテストが配列になって保存されているのでそれらをクリアする
+    ThanxMailer.with(name: user.name, to: user.email).welcome(user).deliver_now # メールを送る処理
   end
 
   describe "ActionMailerの送信'" do
-    describe "ユーザー登録時のメール確認"
+    describe 'ユーザー登録時のメール確認'
 
-    it "指定したアドレスからメールが送信されていること" do
-      expect(ActionMailer::Base.deliveries.last.from.first).to eq("insectfood.info@gmail.com")
+    it '指定したアドレスからメールが送信されていること' do
+      expect(ActionMailer::Base.deliveries.last.from.first).to eq('insectfood.info@gmail.com')
     end
 
-    it "指定した送信先のアドレスであること" do
+    it '指定した送信先のアドレスであること' do
       expect(ActionMailer::Base.deliveries.last.to.first).to eq(user.email)
     end
 
-    it "正常にメールが送信されていること" do
-      expect(ActionMailer::Base.deliveries.last.subject).to eq("ご登録ありがとうございます")
+    it '正常にメールが送信されていること' do
+      expect(ActionMailer::Base.deliveries.last.subject).to eq('ご登録ありがとうございます')
     end
   end
 end
