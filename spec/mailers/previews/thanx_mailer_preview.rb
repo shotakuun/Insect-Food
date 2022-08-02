e9bae9ac2bf195a2f27498f616ca932405952ac2
Masaaki618 <masaaki_618@icloud.com>
2022-08-02 19:40:30 +0900

    Add: ユーザー登録後の確認メールテストの追加#25

--- spec/mailers/previews/thanx_mailer_preview.rb
+++ spec/mailers/previews/thanx_mailer_preview.rb
@@ -1,9 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/thanx_mailer
 class ThanxMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/thanx_mailer/welcome
-  def welcome
-    ThanxMailer.welcome
-  end
-
 end
