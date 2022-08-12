f94c4a3b3186d0a973327dcea9ce6f55dc9612ef
Masaaki618 <masaaki_618@icloud.com>
2022-08-12 11:59:00 +0900

    Fix: Insectモデルの追加#40

---
+++ app/models/insect.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: insects
+#
+#  id                 :bigint           not null, primary key
+#  catch_copy         :string(255)
+#  cooking_store_link :text(65535)
+#  insect_img         :string(255)
+#  introduction       :string(255)
+#  name               :string(255)
+#  online_store_link  :text(65535)
+#  video_url_link     :text(65535)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+class Insect < ApplicationRecord
+end
