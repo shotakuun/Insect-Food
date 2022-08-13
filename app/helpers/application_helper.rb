f47ad776dcc329fa1a1d93a77fe9c4f94dd9f8e2
Masaaki618 <masaaki_618@icloud.com>
2022-08-13 16:55:48 +0900

    Add: DBから取得した値を適切なURLに処理をする記載を追加#57

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  require "uri"
+
+  def text_url_to_link(text)
+    require "uri"
+    uri_reg = URI.regexp(%w[http https])
+    return text.gsub(uri_reg) { "<a href='#{$&}' target='_blank'\>#{$&}</a>" }
+  end
+
   BASE_TITLE = Settings.static.title
+
   def full_title(page_title)
     page_title.blank? ? BASE_TITLE : "#{BASE_TITLE} | #{page_title}"
   end
